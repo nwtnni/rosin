a9e368ccfd9e32eca138ac3ca9feead9326f58c8
Newton Ni <nwtnni@gmail.com>
2020-06-27 16:26:14 -0400

    Set up minimal compiling binary

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+#![no_main]
+#![no_std]
+
+use core::panic;
+
+#[panic_handler]
+fn panic(_: &panic::PanicInfo) -> ! {
+    loop {}
+}
+
+#[no_mangle]
+pub extern "C" fn _start() {
+    loop {}
 }
