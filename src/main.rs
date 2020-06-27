8cdd60fad59c5d369342c7d7a7daf2131e22e72f
Newton Ni <nwtnni@gmail.com>
2020-06-27 17:39:50 -0400

    Implement macro printing interface

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 #![no_main]
 #![no_std]
 
-use core::fmt::Write as _;
 use core::panic;
 
-use rosin::vga;
+use rosin::println;
 
 #[panic_handler]
-fn panic(_: &panic::PanicInfo) -> ! {
+fn panic(info: &panic::PanicInfo) -> ! {
+    println!("{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() {
-    let mut writer = vga::screen::WRITER.lock();
-    writeln!(writer, "Hello, wörld!").unwrap();
-    writeln!(writer, "The numbers are {} and {}", 42, 1.0/3.0).unwrap();
+    println!("Hello, wörld!");
+    println!("The numbers are {} and {}", 42, 1.0/3.0);
+    panic!("Here");
 }
--- src/vga.rs
+++ src/vga.rs
@@ -1,2 +1,21 @@
+use core::fmt;
+use core::fmt::Write as _;
+
 pub mod color;
 pub mod screen;
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    screen::WRITER.lock().write_fmt(args).unwrap();
+}
