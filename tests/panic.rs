1bd0ce11137d966f6a03515f3185774dd0ac8ba9
Newton Ni <nwtnni@gmail.com>
2020-07-05 19:30:26 -0400

    Add panicking integration test

---
+++ tests/panic.rs
@@ -0,0 +1,28 @@
+#![no_main]
+#![no_std]
+
+use core::panic;
+
+use rosin::sprint;
+use rosin::sprintln;
+use rosin::qemu;
+
+#[no_mangle]
+extern "C" fn _start() -> ! {
+    should_fail();
+    qemu::exit(qemu::Exit::Failure);
+    loop {}
+}
+
+#[panic_handler]
+fn panic(info: &panic::PanicInfo) -> ! {
+    sprintln!("[ok]");
+    qemu::exit(qemu::Exit::Success);
+    loop {}
+}
+
+fn should_fail() {
+    sprint!("should_panic::should_fail...\t");
+    assert_eq!(0, 1);
+    sprintln!("[failed]");
+}
