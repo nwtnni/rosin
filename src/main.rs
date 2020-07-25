696835fd843913cacbe187ebf7608efee607789a
Newton Ni <nwtnni@gmail.com>
2020-07-25 14:37:31 -0400

    Move stack overflow to integration test

--- src/main.rs
+++ src/main.rs
@@ -15,13 +15,6 @@ extern "C" fn _start() -> ! {
 
     rosin::init();
 
-    #[allow(unconditional_recursion)]
-    fn stack_overflow() {
-        stack_overflow();
-    }
-
-    stack_overflow();
-
     #[cfg(test)]
     test_main();
 
---
+++ tests/stack_overflow.rs
@@ -0,0 +1,57 @@
+#![no_std]
+#![no_main]
+#![feature(abi_x86_interrupt)]
+
+use core::panic;
+
+use lazy_static::lazy_static;
+use rosin::sprint;
+use rosin::sprintln;
+use rosin::qemu;
+use x86_64::structures::idt;
+
+lazy_static! {
+    static ref IDT: idt::InterruptDescriptorTable = {
+        let mut idt = idt::InterruptDescriptorTable::new();
+        unsafe {
+            idt.double_fault
+                .set_handler_fn(double_fault)
+                .set_stack_index(rosin::gdt::DOUBLE_FAULT);
+        }
+        idt
+    };
+}
+
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    sprint!("stack_overflow::stack_overflow...\t");
+
+    rosin::gdt::init();
+    IDT.load();
+
+    stack_overflow();
+
+    sprintln!("[failed]");
+    panic!("Execution continued after stack overflow");
+}
+
+#[allow(unconditional_recursion)]
+fn stack_overflow() {
+    stack_overflow();
+    // Prevent tail-call optimizations
+    volatile::Volatile::new(0).read();
+}
+
+#[panic_handler]
+fn panic(info: &panic::PanicInfo) -> ! {
+    rosin::panic(info)
+}
+
+extern "x86-interrupt" fn double_fault(
+    _: &mut idt::InterruptStackFrame,
+    _: u64,
+) -> ! {
+    sprintln!("[ok]");
+    qemu::exit(qemu::Exit::Success);
+    loop {}
+}
