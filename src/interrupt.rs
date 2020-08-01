adccbe4ddb4512b0adc1190f9d7788ad03273106
Newton Ni <nwtnni@gmail.com>
2020-08-01 12:43:14 -0400

    Read keyboard scancodes in interrupt handler

--- src/interrupt.rs
+++ src/interrupt.rs
@@ -1,5 +1,6 @@
 use lazy_static::lazy_static;
 use pic8259_simple::ChainedPics;
+use x86_64::instructions::port;
 use x86_64::structures::idt;
 
 use crate::gdt;
@@ -20,7 +21,8 @@ lazy_static! {
         let mut idt = idt::InterruptDescriptorTable::new();
         idt.breakpoint.set_handler_fn(breakpoint);
 
-        idt[Index::Timer.as_usize()].set_handler_fn(timer_interrupt);
+        idt[Index::Timer.as_usize()].set_handler_fn(timer);
+        idt[Index::Keyboard.as_usize()].set_handler_fn(keyboard);
 
         unsafe {
             idt.double_fault
@@ -46,6 +48,7 @@ pub fn init_pics() {
 #[derive(Copy, Clone, Debug, PartialEq, Eq)]
 pub enum Index {
     Timer = PIC_1_OFFSET,
+    Keyboard = PIC_1_OFFSET + 1,
 }
 
 impl Index {
@@ -54,8 +57,21 @@ impl Index {
     }
 }
 
-extern "x86-interrupt" fn timer_interrupt(_stack_frame: &mut idt::InterruptStackFrame) {
+extern "x86-interrupt" fn timer(_stack_frame: &mut idt::InterruptStackFrame) {
     print!(".");
+    unsafe {
+        PICS.lock()
+            .notify_end_of_interrupt(Index::Timer as u8);
+    }
+}
+
+extern "x86-interrupt" fn keyboard(_stack_frame: &mut idt::InterruptStackFrame) {
+    let scancode: u8 = unsafe {
+        port::Port::new(0x60).read()
+    };
+
+    print!("{}", scancode);
+
     unsafe {
         PICS.lock()
             .notify_end_of_interrupt(Index::Timer as u8);
