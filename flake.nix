ecd12117bd25cc582f78b76fd07cbd151733d275
Newton Ni <nwtnni@gmail.com>
2025-05-23 11:35:45 -0500

    Fix cross-compilation environment

--- flake.nix
+++ flake.nix
@@ -12,17 +12,30 @@
   outputs = { self, nixpkgs, flake-utils, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        rustToolchain = pkgs.pkgsBuildHost.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-      in
-      with pkgs; {
-        devShells.default = mkShell {
-          nativeBuildInputs = [ rustToolchain ];
-          buildInputs = [ ];
-        };
+        # https://github.com/NixOS/nixpkgs/blob/c9e5b2da15d68680c7b697bb6ecb1d2f86dfc6d3/lib/systems/examples.nix#L248
+        pkgs = (import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        });
+        pkgsCross = pkgs.pkgsCross.aarch64-embedded;
+      in {
+        # https://nix.dev/tutorials/cross-compilation.html#developer-environment-with-a-cross-compiler
+        # https://github.com/NixOS/nixpkgs/issues/190289
+        devShells.default = pkgsCross.callPackage (
+          { mkShell, qemu }:
+          mkShell {
+            nativeBuildInputs = [
+              # https://ryantm.github.io/nixpkgs/stdenv/cross-compilation/#sec-cross-infra
+              (pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml)
+            ];
+
+            # Maybe the clearest explanation of the Nix cross-compilation model:
+            # https://nixos.org/manual/nixpkgs/stable/#ssec-stdenv-dependencies-propagated
+            depsBuildBuild = [
+              qemu
+            ];
+          }
+        ) {};
       }
     );
 }
