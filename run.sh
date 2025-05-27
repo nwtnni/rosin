d6461f4d266b3eb340a6386a355d35900ad77887
Newton Ni <nwtnni@gmail.com>
2025-05-27 13:16:49 -0500

    Set up console

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -o pipefail
+set -o nounset
+set -o errexit
+
+cargo build
+
+qemu-system-aarch64 \
+    -machine raspi3b \
+    -display none \
+    -serial stdio \
+    -kernel ./target/aarch64-unknown-none-softfloat/debug/rosin
--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,50 @@
 #![no_std]
 
+use core::fmt::Write;
+
 use aarch64_cpu::asm;
 
 pub fn spin() -> ! {
     loop {
         asm::wfe()
+    }
+}
+
+#[doc(hidden)]
+pub fn _print(args: core::fmt::Arguments) {
+    Console.write_fmt(args).unwrap()
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => {
+        $crate::_print(format_args!($($arg)*))
+    };
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => {
+        {
+            $crate::_print(format_args!($($arg)*));
+            $crate::print!("\n");
+        }
+    };
+}
+
+struct Console;
+
+impl Write for Console {
+    fn write_str(&mut self, string: &str) -> core::fmt::Result {
+        const ADDRESS: *mut u8 = 0x3F20_1000 as _;
+
+        for byte in string.as_bytes() {
+            unsafe {
+                ADDRESS.write_volatile(*byte);
+            }
+        }
+
+        Ok(())
     }
 }
