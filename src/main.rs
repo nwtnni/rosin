e183ecdc5131eb4dd845a4e3dae3b80ec6b37a8b
Newton Ni <nwtnni@gmail.com>
2025-05-27 08:25:23 -0500

    Checkpoint work on initialization

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,52 @@
 #![no_std]
 #![no_main]
 
+// https://devblogs.microsoft.com/oldnewthing/20220726-00/?p=106898
+// https://dinfuehr.github.io/blog/encoding-of-immediate-values-on-aarch64/
+// https://developer.arm.com/documentation/dui0774/i/armclang-Integrated-Assembler-Directives
+// https://stackoverflow.com/questions/38570495/aarch64-relocation-prefixes/38608738#38608738
 core::arch::global_asm! {
-    "
-        .pushsection .text._start
+"
+.pushsection .text._start
 
-        _start:
-        wfe
-        b _start
+_start:
+    mrs x0, MPIDR_EL1
+    and x0, x0, 0b11
+    cmp x0, xzr
+    b.ne .L_loop
 
-        .global _start
-        .size _start, . - _start
-        .type _start, %function
-        .popsection
-    "
+    # Explicit :pg_hi21: doesn't seem to be supported
+    # https://reviews.llvm.org/D64455
+    adrp x0, __BSS_LO
+    add x0, x0, :lo12:__BSS_LO
+
+    adrp x1, __BSS_HI
+    add x1, x1, :lo12:__BSS_HI
+.L_bss:
+    cmp x0, x1
+    b.eq .L_rust
+    stp xzr, xzr, [x0], 16
+    b .L_bss
+.L_rust:
+    adr x0, __STACK_HI
+    mov sp, x0
+
+.L_loop:
+    wfe
+    b .L_loop
+
+.size _start, . - _start
+.type _start, %function
+.global _start
+.popsection
+"
 }
 
 use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    unimplemented!()
+    loop {
+        aarch64_cpu::asm::wfe();
+    }
 }
