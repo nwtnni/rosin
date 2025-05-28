b35c9d10bb2ae89fb0c637b37fba32efcfcb0e95
Newton Ni <nwtnni@gmail.com>
2025-05-28 14:20:53 -0500

    Drop to execution level 1

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,22 @@
 #![no_std]
 #![no_main]
 
+use core::fmt::Write as _;
+
+use aarch64_cpu::asm;
+use aarch64_cpu::registers::CNTHCTL_EL2;
+use aarch64_cpu::registers::CNTVOFF_EL2;
+use aarch64_cpu::registers::CurrentEL;
+use aarch64_cpu::registers::ELR_EL2;
+use aarch64_cpu::registers::ELR_EL3;
+use aarch64_cpu::registers::HCR_EL2;
+use aarch64_cpu::registers::SCR_EL3;
+use aarch64_cpu::registers::SP_EL1;
+use aarch64_cpu::registers::SPSR_EL2;
+use aarch64_cpu::registers::SPSR_EL3;
+use tock_registers::interfaces::Readable as _;
+use tock_registers::interfaces::Writeable as _;
+
 // https://devblogs.microsoft.com/oldnewthing/20220726-00/?p=106898
 // https://dinfuehr.github.io/blog/encoding-of-immediate-values-on-aarch64/
 // https://developer.arm.com/documentation/dui0774/i/armclang-Integrated-Assembler-Directives
@@ -32,7 +48,7 @@ _start:
 .L_rust:
     ADR_REL x0, __STACK_HI
     mov sp, x0
-    b _start_kernel
+    b _start_hypervisor
 .L_loop:
     wfe
     b .L_loop
@@ -45,6 +61,48 @@ _start:
 }
 
 #[unsafe(no_mangle)]
+extern "C" fn _start_hypervisor(stack_hi: u64) -> ! {
+    let level = CurrentEL.read(CurrentEL::EL);
+
+    if level >= 3 {
+        SCR_EL3.write(SCR_EL3::RW::NextELIsAarch64);
+    }
+
+    if level >= 2 {
+        CNTHCTL_EL2.write(CNTHCTL_EL2::EL1PCEN::SET + CNTHCTL_EL2::EL1PCTEN::SET);
+        CNTVOFF_EL2.set(0);
+        HCR_EL2.write(HCR_EL2::RW::EL1IsAarch64);
+    }
+
+    #[allow(clippy::fn_to_numeric_cast)]
+    match level {
+        2 => {
+            ELR_EL2.set(_start_kernel as u64);
+            SPSR_EL2.write(
+                SPSR_EL2::D::Masked
+                    + SPSR_EL2::A::Masked
+                    + SPSR_EL2::I::Masked
+                    + SPSR_EL2::F::Masked
+                    + SPSR_EL2::M::EL1h,
+            );
+        }
+        3 => {
+            ELR_EL3.set(_start_kernel as u64);
+            SPSR_EL3.write(
+                SPSR_EL3::D::Masked
+                    + SPSR_EL3::A::Masked
+                    + SPSR_EL3::I::Masked
+                    + SPSR_EL3::F::Masked
+                    + SPSR_EL3::M::EL1h,
+            );
+        }
+        _ => _start_kernel(),
+    }
+
+    SP_EL1.set(stack_hi);
+    asm::eret()
+}
+
 fn _start_kernel() -> ! {
     rosin::initialize();
     rosin::info!("Hello, world!");
